3c2bccbe5042785a9a1d1987392b938873a54375
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-10-25 01:39:21 +0200

    weather data munger parses line containing weather data

--- test_weather_data_munging.rb
+++ test_weather_data_munging.rb
@@ -34,4 +34,11 @@ class TestWeatherDataMunger < Test::Unit::TestCase
     mungedData = WeatherDataMunger.new.munge(rawData)
     assert_equal([], mungedData)
   end
+  def test_parses_line_containing_weather_data
+    rawData = "  Dy MxT   MnT   AvT   HDDay  AvDP 1HrP TPcpn WxType PDir AvSp Dir MxS SkyC MxR MnR AvSLP\n" +
+              "\n" +
+              "   1  88    59    74          53.8       0.00 F       280  9.6 270  17  1.6  93 23 1004.5\n"
+    mungedData = WeatherDataMunger.new.munge(rawData)
+    assert_equal([DayWeatherData.new(1, 59, 88)], mungedData)
+  end
 end
--- weather_data_munging.rb
+++ weather_data_munging.rb
@@ -12,12 +12,24 @@ end
 
 class WeatherDataMunger
   def munge data
-    []
+    mungedData = []
+    data.each_line do |line|
+      if not(line.empty?) and not(line[0..4].strip.to_i == 0)
+          day = line[0..4].strip.to_i
+          maxTemp = line[5..8].strip.to_i
+          minTemp = line[9..14].strip.to_i
+          mungedData << DayWeatherData.new(day,minTemp,maxTemp)
+      end
+    end
+    mungedData
   end
 end
 
 class DayWeatherData
+  include Comparable
+
   attr_reader :day, :minTemp, :maxTemp
+  
   def initialize(day, minTemp, maxTemp)
     @day = day
     @minTemp = minTemp
@@ -25,5 +37,15 @@ class DayWeatherData
   end
   def tempStread
     @maxTemp - @minTemp
+  end
+
+  def <=>(other)
+    if self.tempStread < other.tempStread
+      -1
+    elsif self.tempStread > other.tempStread
+      1
+    else
+      0
+    end
   end
 end
