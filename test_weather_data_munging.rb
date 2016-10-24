ebd6b24563c8a200d389bb60363fd46ffe2b2ee6
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-10-25 00:46:26 +0200

    weather data munger ignores empty line

--- test_weather_data_munging.rb
+++ test_weather_data_munging.rb
@@ -24,7 +24,13 @@ end
 
 class TestWeatherDataMunger < Test::Unit::TestCase
   def test_ignores_header
-    rawData = '  Dy MxT   MnT   AvT   HDDay  AvDP 1HrP TPcpn WxType PDir AvSp Dir MxS SkyC MxR MnR AvSLP'
+    rawData = "  Dy MxT   MnT   AvT   HDDay  AvDP 1HrP TPcpn WxType PDir AvSp Dir MxS SkyC MxR MnR AvSLP"
+    mungedData = WeatherDataMunger.new.munge(rawData)
+    assert_equal([], mungedData)
+  end
+  def test_ignores_empty_line
+    rawData = "  Dy MxT   MnT   AvT   HDDay  AvDP 1HrP TPcpn WxType PDir AvSp Dir MxS SkyC MxR MnR AvSLP\n" +
+              "\n"
     mungedData = WeatherDataMunger.new.munge(rawData)
     assert_equal([], mungedData)
   end
