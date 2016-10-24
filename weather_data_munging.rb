84dc0f96911d9a27d71daed61312aa6036c46c29
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-10-25 01:43:04 +0200

    refactor to substitute if with unless

--- weather_data_munging.rb
+++ weather_data_munging.rb
@@ -14,11 +14,13 @@ class WeatherDataMunger
   def munge data
     mungedData = []
     data.each_line do |line|
-      if not(line.empty?) and not(line[0..4].strip.to_i == 0)
-          day = line[0..4].strip.to_i
+      unless line.empty?
+        day = line[0..4].strip.to_i
+        unless day == 0
           maxTemp = line[5..8].strip.to_i
           minTemp = line[9..14].strip.to_i
           mungedData << DayWeatherData.new(day,minTemp,maxTemp)
+        end
       end
     end
     mungedData
