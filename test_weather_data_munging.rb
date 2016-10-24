a266ddbf07fe64c3b5c562eb56f7ad7cd14eda80
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-10-25 00:20:10 +0200

    weather data munger returns day from single day weather data

--- test_weather_data_munging.rb
+++ test_weather_data_munging.rb
@@ -8,4 +8,9 @@ class TestWeatherMunger < Test::Unit::TestCase
       WeatherDataMunger.new.munge(emptyData)
     end
   end
+  def test_returns_day_from_single_day_weather_data
+    weatherData = [DayWeatherData.new(1, 20, 30)]
+    smallestTemperatureSpreadDay = WeatherDataMunger.new.munge(weatherData)
+    assert_equal(1, smallestTemperatureSpreadDay)
+  end
 end
--- weather_data_munging.rb
+++ weather_data_munging.rb
@@ -2,6 +2,19 @@ require "./exceptions.rb"
 
 class WeatherDataMunger
   def munge data
-    raise Exceptions::EmptyData
+    if data.empty?
+      raise Exceptions::EmptyData
+    else
+      1
+    end
+  end
+end
+
+class DayWeatherData
+  attr_reader :day, :minTemp, :maxTemp
+  def initialize(day, minTemp, maxTemp)
+    @day = day
+    @minTemp = minTemp
+    @maxTemp = maxTemp
   end
 end
